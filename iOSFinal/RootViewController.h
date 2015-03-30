470055069a765a95fb022094159c1b90eafc5e04
Robin Verheijen <info@verrodesigns.nl>
2015-03-30 14:15:54 +0200

    Check if the user has already used the app

---
+++ iOSFinal/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  iOSFinal
+//
+//  Created by Robin Verheijen on 30-03-15.
+//  Copyright (c) 2015 Saxion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITabBarController
+
+@end
